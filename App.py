23f262482913e31703dec34f6a66bd16ea14b5b4
zastuck <zastuck@gmail.com>
2026-05-17 00:35:27 +0200

    Update exemptions

--- App.py
+++ App.py
@@ -126,7 +126,7 @@ def APOD_dialog():
             <h4 style="margin: 0 0 1.2vw 0; font-size: clamp(0.9rem, 2.5vw, 1.4rem);">{apod['title']}</h4>
             <h6 style="margin: 0 0 1vw 0; font-size: clamp(0.7rem, 1.5vw, 1rem);">{pub_date.strftime("%A, %B %d, %Y")}</h6>
             <p class="apod-explanation" style="margin: 0 0 1vw 0; font-size: clamp(0.6rem, 1.2vw, 0.85rem);">{apod['explanation']}</p>
-            <p style="margin: 0; font-size: clamp(0.6rem, 1.2vw, 0.85rem);">{apod['credit']}</p>
+            <p class="apod-explanation" style="margin: 0; font-size: clamp(0.6rem, 1.2vw, 0.85rem);">{apod['credit']}</p>
         </div>
     </div>
     """,
